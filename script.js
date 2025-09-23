cb7ecb86f8beec13d9748dca9d0f24b5de494f2f
Elton Salanic <elton.salanic@gmail.com>
2025-09-23 12:48:20 -0700

    saved small styling changes

--- script.js
+++ script.js
@@ -18,17 +18,18 @@ async function getData() {
 async function renderProducts() {
   //get products and imageUrl
   const products = await getData();
-  //const imageUrl = products[0].images[0].url;
 
   //for every product, add it to the div;
-  const productsContainer = document.getElementById("products");
+  const productsDisplayContainer = document.getElementById("merch-display-container");
   for (let i = 0; i < products.length; i++) {
-    console.log("loop " + i);
+    const productContainer = document.createElement("a"); //create div for a product
+    productContainer.setAttribute("href", (storeUrl + "product/" + products[i].permalink));
+    productContainer.setAttribute("class", "product-container"); //set product div's class to product-container
     const imageUrl = products[i].images[0].url; //get image for product
-    const imageElementToAdd = document.createElement("img"); //create image
-    imageElementToAdd.setAttribute("src", imageUrl); //set shit
-    imageElementToAdd.setAttribute("class", "productImage");
-    productsContainer.appendChild(imageElementToAdd); //add to div
+    const imageElementToAdd = document.createElement("img"); //create image element
+    imageElementToAdd.setAttribute("src", imageUrl); //put image url into img element
+    productContainer.appendChild(imageElementToAdd); //put image in div
+    productsDisplayContainer.appendChild(productContainer); //append product-container into merch-displaycontainer;
   }
   //products grabbed atp
   //Render products onto page
@@ -40,7 +41,7 @@ async function renderProducts() {
   );*/
 }
 
-/*renderProducts();*/
+renderProducts();
 
 
 //var elem = document.createElement("img");
