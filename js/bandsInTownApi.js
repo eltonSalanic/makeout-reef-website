bd75b8fb995aee41cce54cded0010df7198f3e0c
Elton Salanic <eltonsalanic@MacBookPro.lan>
2025-10-20 20:07:05 -0700

    shows are fetched and displayed properly

--- js/bandsInTownApi.js
+++ js/bandsInTownApi.js
@@ -7,11 +7,52 @@ async function getShowsData() {
       const response = await fetch(`https://rest.bandsintown.com/artists/makeoutreef/events?${parameters}`);
       if (!response.ok) throw new Error('Network response was not ok');
       const data = await response.json();
-      console.log(data);
+      return data;
     } catch (err) {
       console.error('Fetch failed:', err);
     }
+    
   }
   
-  getShowsData();
+  async function displayShowsData(){
+    const showsData = await getShowsData();
+
+    const showsContainer = document.querySelector('.shows-list');
+
+    showsData.forEach(show =>{
+        //format date
+        const date = new Date(show.datetime);
+        const formattedDate = date.toLocaleDateString('en-US', {
+          weekday: 'short',
+          month: 'short',
+          day: '2-digit'
+        });
+        
+        const showCard = document.createElement('div');
+        showCard.classList.add('show-card');
+        showCard.innerHTML = `
+        <div class="show-where">
+            <div class="show-city">${show.venue.location}</div>
+            <div class="show-venue">${show.venue.name}</div>
+        </div>
+        <div class="show-date">${formattedDate}</div>
+            <div class="show-cta"><a href="${show.url}">tickets</a>
+        </div>
+        `
+        showsContainer.appendChild(showCard);
+        //Fri, Nov 07
+    });
+
+    /*<div class="show-card">
+        <div class="show-where">
+            <div class="show-city">Los Angeles, CA</div>
+            <div class="show-venue">The Echo</div>
+        </div>
+        <div class="show-date">Fri, Nov 07</div>
+            <div class="show-cta"><a href="#">tickets</a>
+        </div>
+    </div>*/
+  }
+
+  displayShowsData();
   
