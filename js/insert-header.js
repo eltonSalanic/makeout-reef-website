a9827ce1e43df3bd67115071ca156ab5eee6c093
Elton Salanic <elton.salanic@gmail.com>
2025-10-28 19:03:07 -0700

    switched to includes for partials. PREVIOUS COMMIT DID NOT FIX PARTIALS NOT BEING INCLUDED
    this new version uses posthtml-include. check Parcel docs in the HTML section to see how it works

--- js/insert-header.js
+++
@@ -1,5 +0,0 @@
-fetch('../partials/header.html')
-            .then(res => res.text())
-            .then(htmlText =>{
-                document.querySelector('.header').innerHTML = htmlText;
-            })
